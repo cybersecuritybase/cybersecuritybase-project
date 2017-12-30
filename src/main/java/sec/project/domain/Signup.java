754beedc9aad34f61047bc328d6ef2928c2c665d
Dmitri9149 <34885758+Dmitri9149@users.noreply.github.com>
2017-12-30 16:06:19 +0200

    Update (add a comment to Signup.java)

--- src/main/java/sec/project/domain/Signup.java
+++ src/main/java/sec/project/domain/Signup.java
@@ -2,7 +2,7 @@ package sec.project.domain;
 
 import javax.persistence.Entity;
 import org.springframework.data.jpa.domain.AbstractPersistable;
-////////////////////
+////////////////////////
 
 @Entity
 public class Signup extends AbstractPersistable<Long> {
