2394d0df054f05acb98a996534492bfa0b9dcb19
dj1975-moocfi <github-dj1975moocfi@cydonia.dj1975.xinit.se>
2018-12-24 15:13:23 +0100

    initial files from local

--- src/main/java/sec/project/repository/SignupRepository.java
+++
@@ -1,8 +0,0 @@
-package sec.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sec.project.domain.Signup;
-
-public interface SignupRepository extends JpaRepository<Signup, Long> {
-
-}
